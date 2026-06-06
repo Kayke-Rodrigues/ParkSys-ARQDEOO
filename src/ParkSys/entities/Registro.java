05864b353f88eb1f35f965e07af59e8855956c2b
Kayke <kayke.rodriguesr@hotmail.com>
2026-06-06 13:03:08 -0300

    feat(S01): implementa Serializable, Comparable e campo transient em Registro

--- src/ParkSys/entities/Registro.java
+++ src/ParkSys/entities/Registro.java
@@ -2,15 +2,22 @@ package ParkSys.entities;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
-public class Registro implements Serializable, Comparable<Registro>{
-     private static final long serialVersionUID = 1L;
+public class Registro implements Serializable, Comparable<Registro> {
+
+    private static final long serialVersionUID = 1L;
 
     private Veiculo veiculo;
     private LocalDateTime dataEntrada;
     private LocalDateTime dataSaida;
     private double valorPago;
     private String idVaga;
+
+    // Lista de vagas extras ocupadas por SUV (1 extra) e Caminhão (2 extras)
+    private List<String> vagasExtras;
+
+    
     private transient String threadOrigem;
 
     public Registro(Veiculo veiculo, String idVaga) {
@@ -25,7 +32,7 @@ public class Registro implements Serializable, Comparable<Registro>{
     }
 
     public String getIdVaga() {
-    return idVaga;
+        return idVaga;
     }
 
     public Veiculo getVeiculo() {
@@ -59,5 +66,12 @@ public class Registro implements Serializable, Comparable<Registro>{
     public void setThreadOrigem(String threadOrigem) {
         this.threadOrigem = threadOrigem;
     }
-    
+
+    public List<String> getVagasExtras() {
+        return vagasExtras;
+    }
+
+    public void setVagasExtras(List<String> vagasExtras) {
+        this.vagasExtras = vagasExtras;
+    }
 }
